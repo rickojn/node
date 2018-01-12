9328f82f764590c8290523a551c76a3dea0ca770
unknown <rickojn@hotmail.com>
2018-01-12 22:24:26 +0000

    server

---
+++ Myserver.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+let requestCount = 0;
+
+const server = http.createServer((req, res) => {
+  requestCount++;
+  console.log('request ' + requestCount+ ' received ...')
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(req.headers['user-agent']);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+//
