06f1cee9196f543362e1068039154a52940817fc
unknown <rickojn@hotmail.com>
2018-10-10 23:41:32 +0100

    test different keys

---
+++ EncryptIt.js
@@ -0,0 +1,57 @@
+var CryptoJS = require("crypto-js")
+
+var plainText = 'Mary had a little lamb'
+console.log('plain text: ', plainText)
+var keyA = 'x5zG8EiUaZoUVJpsTOKB48g52om1tApEPB2nIdeZZGU='
+// var keyA = 'key A'
+var keyB = 'PwU9uyHmdyDI9Mtqy3ElTHhWGKp/8NQU3k8+8rjVCuw='
+// var keyB = 'key B'
+console.log('------');
+console.log('------');
+// Encrypt with key A
+var ciphertextA = CryptoJS.AES.encrypt(plainText, keyA);
+console.log('encrypted with A: ', ciphertextA.toString())
+console.log('------');
+console.log('------');
+var ciphertextB = CryptoJS.AES.encrypt(plainText, keyB);
+console.log('encrypted with B: ', ciphertextA.toString())
+console.log('------');
+console.log('------');
+
+
+
+// Decrypt
+var bytes  = CryptoJS.AES.decrypt(ciphertextA.toString(), keyA);
+var decryptedTextA = bytes.toString(CryptoJS.enc.Utf8);
+
+console.log('decryptedText: ', decryptedTextA);
+console.log('------');
+console.log('------');
+
+// Decrypt
+var bytesAB  = CryptoJS.AES.decrypt(ciphertextA.toString(), keyB);
+var decryptedTextAB = bytes.toString(CryptoJS.enc.Utf8);
+console.log('decrypted with wrong key Text: ', decryptedTextAB);
+console.log('------');
+console.log('------');
+
+
+// encrypt cipher-text A with Key B:
+var ciphertextAB = CryptoJS.AES.encrypt(ciphertextA.toString(), keyB);
+console.log('double encrypted ciphertext: ', ciphertextAB.toString())
+console.log('------');
+console.log('------');
+
+// decrypt cipher-text AB with key A:
+var decryptedWithA = CryptoJS.AES.decrypt(ciphertextAB.toString(), keyA).toString(CryptoJS.enc.Utf8);
+// now decrypt it with key B:
+var decryptedWithB = CryptoJS.AES.decrypt(decryptedWithA.toString(), keyA).toString(CryptoJS.enc.Utf8);
+console.log('------');
+console.log('------');
+
+console.log('decrypted by keys A and B:  ', decryptedWithB);
+
+
+
+
+
